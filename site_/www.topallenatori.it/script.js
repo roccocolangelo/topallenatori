a32cc78bc4dbbefc9b142ac3d75fe50208a9dec2
Colangelo <rocco.colangelo@ing.com>
2025-03-28 14:59:04 +0100

    carousel

--- site_/www.topallenatori.it/script.js
+++ site_/www.topallenatori.it/script.js
@@ -1,3 +1,28 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Top Allenatori homepage loaded!');
-});
+let currentSlide = 0;
+const slides = document.querySelectorAll('.carousel-inner img');
+
+function showSlide(index) {
+    const totalSlides = slides.length;
+    if (index >= totalSlides) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = totalSlides - 1;
+    } else {
+        currentSlide = index;
+    }
+    const offset = -currentSlide * 100;
+    document.querySelector('.carousel-inner').style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
+
+showSlide(currentSlide);
+
+// Automatically slide every 2 seconds
+setInterval(nextSlide, 4000);
