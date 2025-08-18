ce60acca35bbfe1d2deba1eb87b3507c8c308c51
Colangelo <rocco.colangelo@ing.com>
2025-08-18 11:13:19 +0200

    update domain refer

--- xmlrpc0db0.php
+++ xmlrpc0db0.php
@@ -2,13 +2,13 @@
   <service>
     <engineName>WordPress</engineName>
     <engineLink>https://wordpress.org/</engineLink>
-    <homePageLink>https://www.topallenatori.it</homePageLink>
+    <homePageLink>https://topallenatori.com</homePageLink>
     <apis>
-      <api name="WordPress" blogID="1" preferred="true" apiLink="https://www.topallenatori.it/xmlrpc.php" />
-      <api name="Movable Type" blogID="1" preferred="false" apiLink="https://www.topallenatori.it/xmlrpc.php" />
-      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://www.topallenatori.it/xmlrpc.php" />
-      <api name="Blogger" blogID="1" preferred="false" apiLink="https://www.topallenatori.it/xmlrpc.php" />
-      	<api name="WP-API" blogID="1" preferred="false" apiLink="https://www.topallenatori.it/wp-json/" />
+      <api name="WordPress" blogID="1" preferred="true" apiLink="https://topallenatori.com/xmlrpc.php" />
+      <api name="Movable Type" blogID="1" preferred="false" apiLink="https://topallenatori.com/xmlrpc.php" />
+      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://topallenatori.com/xmlrpc.php" />
+      <api name="Blogger" blogID="1" preferred="false" apiLink="https://topallenatori.com/xmlrpc.php" />
+      	<api name="WP-API" blogID="1" preferred="false" apiLink="https://topallenatori.com/wp-json/" />
 	    </apis>
   </service>
 </rsd>
